be5c330c8d1cd3aa367e7f99dbfc3c559b2eceae
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-01-23 17:11:11 -0500

    More filtering

--- activity_filter.py
+++ activity_filter.py
@@ -1,5 +1,11 @@
+from difflib import SequenceMatcher as sm
+
 def get_types():
 	with open("types.txt") as t:
 		return t.read().split()
 
-print(get_types())
+def top_3_matches(user_idea):
+	similarities = [[t, sm(None, user_idea, t).ratio()] for t in get_types()]
+	print(similarities)
+
+print(top_3_matches("bike"))
