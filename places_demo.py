e1837b461635c805408b1e28585b58537e702157
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-01-23 16:13:46 -0500

    Fix popen issue

--- places_demo.py
+++ places_demo.py
@@ -2,11 +2,10 @@ import os, requests
 
 def loc_data(key, name, loc_type, radius = 500):
 	API_BASE = f"""https://maps.googleapis.com/maps/api/place/nearbysearch/json?
-	location={os.popen('curl ipinfo.io/loc')}&radius={radius}&types={loc_type}&name={name}&key={key}
+	location={os.popen('curl ipinfo.io/loc').read()}&radius={radius}&types={loc_type}&name={name}&key={key}
 	"""
-
-	return requests.get(API_BASE).text
-
+	print(API_BASE)
+	return requests.post(API_BASE).text
 
 my_key = "AIzaSyB2TMvSqQ9or3Q4YqgGdnRHcX6lgPhYiJU"
-print(loc_data(my_key, "harbor", "restaurant"))
+print(loc_data(my_key, "food", "harbour"))
