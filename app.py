165135e598f1759fe050ec35c9b8abcc78b9da51
NuoWenLei <nuowen0612@gmail.com>
2021-01-25 01:27:27 +0800

    made api filter search, right now it prints to console

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 from db_construction import Activity, Placetype, Comment, Action, Mood_and_Activity, Mood
 from helper import vector_cosine_similarity, getPlaces
+from activity_filter import nearby_locs_from_type
 import requests, json, datetime, nltk, ssl, spacy, en_core_web_md, math
 
 try:
@@ -21,11 +22,16 @@ nlp = en_core_web_md.load()
 
 app = Flask(__name__)
 
+with open('config.json') as json_data:
+	config_file = json.load(json_data)
+
+API_KEY = config_file['apikey']
+
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config['SESSION_PERMANENT'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hackathon2021.db'
 db = SQLAlchemy(app)
-app.secret_key = 'somethingsecret'
+app.secret_key = config_file['secretkey']
 app.config["SESSION_TYPE"] = 'filesystem'
 
 
@@ -37,9 +43,9 @@ def index():
 		#checklist will be all the selected activites
 		checkList = []
 		if request.form.get('activityCheck') == 'on':
-		    for activity_check in request.form.keys():
-		        if 'typeCheck' in activity_check and request.form.get(activity_check) == 'on':
-		            checkList.append(activity_check.split('@')[1].strip())
+			for activity_check in request.form.keys():
+				if 'typeCheck' in activity_check and request.form.get(activity_check) == 'on':
+					checkList.append(activity_check.split('@')[1].strip())
 		radius = 10000
 		if request.form.get('radiusCheck') == 'on':
 			try:
@@ -57,6 +63,20 @@ def index():
 		placetypes = getPlaces(checkList)
 
 		#TODO: GOOGLE API filter by placetypes, radius, price range, and indoor?
+
+		d = {'radius':radius}
+
+		if len(checkList) > 0:
+			d['type'] = '|'.join(placetypes)
+		
+		if price_range is not None:
+			d['minprice'] = price_range
+			d['maxprice'] = price_range
+		
+		if prefer_indoor is not None:
+			d['keyword'] = 'indoor' if prefer_indoor else 'outdoor'
+
+		location_data = json.loads(nearby_locs_from_type(d))
 		
 		return render_template("index.html", activities=Activity.query.order_by(Activity.activity_name).all())
 
@@ -95,8 +115,32 @@ def activity_mood_search():
 	else:
 		return render_template('apologies.html')
 
-# @app.route('/api/v1/filtersearch', methods=['POST'])
-# def filtersearch():
+@app.route('/api/v1/filtersearch', methods=['POST'])
+def filtersearch():
+	if request.method == 'POST':
+		data = request.get_json()
+		d = {'key':API_KEY}
+		if 'activities' in data.keys():
+			d['type'] = '|'.join(getPlaces(data['activites']))
+
+		if 'radius' in data.keys():
+			d['radius'] = int(data['radius']) * 1000
+		else:
+			d['radius'] = 10000
+
+		if 'price_range' in data.keys():
+			d['minprice'] = d['maxprice'] = int(data['price_range'])
+		
+		if 'prefer_indoor' in data.keys():
+			preference = int(data['prefer_indoor'])
+			if preference==1:
+				d['keyword'] = 'indoor'
+			elif preference==2:
+				d['keyword'] = 'outdoor'
+		google_data = json.loads(nearby_locs_from_type(d))
+		return jsonify(google_data)
+	else:
+		return render_template('apologies.html')
 
 
 if __name__=='__main__':
